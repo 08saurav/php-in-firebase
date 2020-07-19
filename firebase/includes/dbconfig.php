f3e7e83525a4175f67ac9c9fc1e399905f36a137
Saurabh Kumar Bhartia <32289064+08saurav@users.noreply.github.com>
2020-07-19 20:53:26 +0530

    Update dbconfig.php

--- firebase/includes/dbconfig.php
+++ firebase/includes/dbconfig.php
@@ -4,10 +4,10 @@ require __DIR__ .'/vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
-$serviceAccount = ServiceAccount::fromJsonFile(__DIR__ .'/test-c422f-firebase-adminsdk-55r34-6519509bf2.json');
+$serviceAccount = ServiceAccount::fromJsonFile(__DIR__ .'example.json');
 $firebase = (new Factory)
     ->withServiceAccount($serviceAccount)
-    ->withDatabaseUri('https://test-c422f.firebaseio.com')
+    ->withDatabaseUri('https://example.firebaseio.com')
     ->create();
 
 $database = $firebase->getDatabase();
