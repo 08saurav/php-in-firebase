9811e2b9defe3d1fbff16f9e25efc103dfb96cc7
08saurav <s.bhartia.sb98@gmail.com>
2020-07-19 15:18:55 +0530

    my commit

---
+++ library management system/library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://iiitg.ac.in/" target="_blank"  > Designed by : IIITG</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
